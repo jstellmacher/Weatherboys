7c24235c7d02f6280e9e741dd1f9f349a4412726
Jai <jaijai.stell@gmail.com>
2023-04-05 16:19:57 -0700

    big changes

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-
-//**started by fetching but still currently working on db.json 
-fetch("http://localhost:3000/cities")
-.then((r) => r.json())
-.then((cityData) => {
-cityData.forEach(city => {
-    console.log(city)
-})
-});
-
-
+//**started by fetching but still currently working on db.json
+// fetch("http://localhost:3000/cities")
+// .then((r) => r.json())
+// .then((cityData) => {
+// cityData.forEach(city => {
+//     console.log(city)
+// })
+// });
+function myFunction() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+// !
+// fetch(
+//   "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+// )
+//   .then((r) => r.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
